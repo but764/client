83e532e1e0ec84441ab89a5f3af95847d3abbbc0
butak <but764@yandex.ru>
2022-01-17 17:48:42 +0400

    Lesson6

--- HW5/message.py
+++ HW5/message.py
@@ -6,42 +6,52 @@ from common.variables import MAX_PACKAGE_LENGTH, ENCODING
 
 
 class Message:
-    """
-    Класс для отсылки и полуения сообщений
-    """
-    @staticmethod
-    def get(socketfrom):
-        '''
-        Утилита приёма и декодирования сообщения
-        принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
-        :param client:
-        :return:
-        '''
+    """ Определяем класс для обработки сообщений"""
 
-        encoded_response = socketfrom.recv(MAX_PACKAGE_LENGTH)
-        if isinstance(encoded_response, bytes):
-            json_response = encoded_response.decode(ENCODING)
-            response = json.loads(json_response)
-            # print(f'Получили сообщение: {json_response}')
-            if isinstance(response, dict):
-                return response
+    import json
+    from common.variables import MAX_PACKAGE_LENGTH, ENCODING
+    from decorators import log
+
+    class Message:
+        """
+        Класс для отсылки и полуения сообщений
+        """
+
+        @staticmethod
+        @log
+        def get(socketfrom):
+            '''
+            Утилита приёма и декодирования сообщения
+            принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
+            :param client:
+            :return:
+            '''
+
+            encoded_response = socketfrom.recv(MAX_PACKAGE_LENGTH)
+            if isinstance(encoded_response, bytes):
+                json_response = encoded_response.decode(ENCODING)
+                response = json.loads(json_response)
+                # print(f'Получили сообщение: {json_response}')
+                if isinstance(response, dict):
+                    return response
+                raise ValueError
             raise ValueError
-        raise ValueError
 
-    @staticmethod
-    def send(socketto, message):
-        '''
-        Утилита кодирования и отправки сообщения
-        принимает словарь и отправляет его
-        :param sock:
-        :param message:
-        :return:
-        '''
+        @staticmethod
+        @log
+        def send(socketto, message):
+            '''
+            Утилита кодирования и отправки сообщения
+            принимает словарь и отправляет его
+            :param sock:
+            :param message:
+            :return:
+            '''
 
-        if not isinstance(message, dict):
-            raise TypeError
+            if not isinstance(message, dict):
+                raise TypeError
 
-        js_message = json.dumps(message)
-        encoded_message = js_message.encode(ENCODING)
-        socketto.send(encoded_message)
-        # print(f'Послали сообщение: {js_message}')
+            js_message = json.dumps(message)
+            encoded_message = js_message.encode(ENCODING)
+            socketto.send(encoded_message)
+            # print(f'Послали сообщение: {js_message}')
